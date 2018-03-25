6ea9f7679ec162af165b564b9177d8ae992694cc
Daniyal Guliev <tzota@yandex-team.ru>
2018-03-25 17:04:07 +0300

    Добавил 2-й вариант в RomanToInteger

--- src/leetcode/RomanToInteger.cs
+++ src/leetcode/RomanToInteger.cs
@@ -4,13 +4,22 @@ namespace  leetcode
 {
     /// <summary>
     /// https://leetcode.com/problems/roman-to-integer/description/
-    /// 
+    ///
     /// Given a roman numeral, convert it to an integer.
     /// Input is guaranteed to be within the range from 1 to 3999.
     /// </summary>
     public class RomanToInteger
-    {        
-        public int RomanToInt(string s) 
+    {
+        public int RomanToInt(string s)
+        {
+            int a = MyWay(s);
+            int b = SmartWay(s);
+            if (a != b) {
+                throw new Exception("Расхождение в вариантах подсчета");
+            }
+            return a;
+        }
+        private int MyWay(string s)
         {
             var str = s.ToUpper();
             if (str.Length == 1) {
@@ -20,13 +29,10 @@ namespace  leetcode
             int sum = 0;
             int i = 0;
 
-            // Есть более умный вариант - просто вычитать curr, если next больше.
-            // Тогда не надо подкручивать счетчик и не надо нервничать на конце,
-            // ибо концевое значение просто безусловно добавляется после цикла
             while(i < str.Length)
             {
                 int curr = CharToInt(str[i]);
-                
+
                 int next = i + 1 == str.Length ? -1 : CharToInt(str[i + 1]);
 
                 if (next > curr) {
@@ -35,12 +41,39 @@ namespace  leetcode
                 } else {
                     sum += curr;
                 }
+
                 i++;
             }
 
             return sum;
         }
 
+        // Есть более умный вариант - просто вычитать curr, если next больше.
+        // Тогда не надо подкручивать счетчик и не надо нервничать на конце,
+        // ибо концевое значение просто безусловно добавляется после цикла
+        private int SmartWay(string s)
+        {
+            var str = s.ToUpper();
+            if (str.Length == 1) {
+                return CharToInt(str[0]);
+            }
+
+            int sum = 0;
+            for(int i = 0; i < str.Length - 1; i++)
+            {
+                int curr = CharToInt(str[i]);
+
+                int next = i + 1 == str.Length ? -1 : CharToInt(str[i + 1]);
+
+                if (curr < next) {
+                    sum -= curr;
+                } else {
+                    sum += curr;
+                }
+            }
+
+            return sum + CharToInt(str[str.Length - 1]);
+        }
         private int CharToInt(char letter)
         {
             switch(letter)
